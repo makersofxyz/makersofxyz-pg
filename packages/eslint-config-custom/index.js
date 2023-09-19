49261061d7b5f4c91c4bf0d47144ea233809d290
benjamin <benschac@gmail.com>
2023-09-19 08:55:59 -0400

    fix eslint issues

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,24 +1,31 @@
-
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
-    ],
-    rules: {
-        "react/react-in-jsx-scope": "off",
-        "react/prop-types": "off",
-        "@typescript-eslint/no-explicit-any": "warn",
-        "@typescript-eslint/no-unused-vars": "warn",
-        "react/no-unescaped-entities": "warn",
-        "react-hooks/rules-of-hooks": "warn",
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react/no-unescaped-entities': 'warn',
+    'react-hooks/rules-of-hooks': 'warn',
+  },
+  settings: {
+    'import/ignore': ['react-native'],
+    react: {
+      version: 'detect',
     },
-    settings: {
-        'import/ignore': ['react-native'],
-        "react": {
-            "version": "detect",
-        },
+  },
+  overrides: [
+    {
+      files: ['.eslintrc.js'],
+      rules: {
+        'no-undef': 'off',
+      },
     },
+  ],
 }
