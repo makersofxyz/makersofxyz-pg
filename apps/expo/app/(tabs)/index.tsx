321a0cb0e555a04847c7b46dcdba9d1408b0d82c
benjamin <benschac@gmail.com>
2023-10-03 13:36:03 -0400

    chore: add env config to expo app

--- apps/expo/app/(tabs)/index.tsx
+++ apps/expo/app/(tabs)/index.tsx
@@ -2,6 +2,7 @@ import { HomeScreen } from 'app/features/home/screen'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Screen() {
+  console.log(process.env.EXPO_PUBLIC_THING, 'hi xxxxx')
   return (
     <SafeAreaView style={{ flex: 1 }} edges={['left', 'right', 'top']}>
       <HomeScreen />
